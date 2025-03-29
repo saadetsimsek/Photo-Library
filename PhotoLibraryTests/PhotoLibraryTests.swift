955c3e4166d57db487813c3b0d7325518c94beeb
saadetsimsek <saadetsimsek13@gmail.com>
2025-03-29 23:11:24 +0300

    Initial Commit

---
+++ PhotoLibraryTests/PhotoLibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryTests.swift
+//  PhotoLibraryTests
+//
+//  Created by Saadet Şimşek on 29/03/2025.
+//
+
+import Testing
+@testable import PhotoLibrary
+
+struct PhotoLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
