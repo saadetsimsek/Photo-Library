ff42dc57faf9ad083d97da1b2cd12e6336714e25
saadetsimsek <saadetsimsek13@gmail.com>
2025-04-29 13:44:07 +0300

    customized tab bar item

--- PhotoLibrary/Module/TabBarModul/Presenter/TabBarViewPresenter.swift
+++ PhotoLibrary/Module/TabBarModul/Presenter/TabBarViewPresenter.swift
@@ -10,12 +10,16 @@ import UIKit
 protocol TabBarViewPresenterProtocol: AnyObject {
     init(view: TabBarViewControllerProtocol)
 
+    var tabs: [UIImage] { get set}
     func buildTabBar()
 }
 
 class TabBarViewPresenter {
 
     weak var view: TabBarViewControllerProtocol?
+    
+    var tabs: [UIImage] = [.house, .camera, .heart]
+    
     
     required init(view: any TabBarViewControllerProtocol) {
         self.view = view
@@ -29,11 +33,11 @@ extension TabBarViewPresenter: TabBarViewPresenterProtocol {
     
     func buildTabBar() {
         let mainScreen = Builder.createMainScreenController()
-        mainScreen.tabBarItem = UITabBarItem(title: "Main", image: UIImage(systemName: "house"), tag: 0)
+    //    mainScreen.tabBarItem = UITabBarItem(title: "Main", image: UIImage(systemName: "house"), tag: 0)
         let cameraScreen = Builder.createCameraController()
-        cameraScreen.tabBarItem = UITabBarItem(title: "Camera", image: UIImage(systemName: "camera"), tag: 1)
+      //  cameraScreen.tabBarItem = UITabBarItem(title: "Camera", image: UIImage(systemName: "camera"), tag: 1)
         let favoriteScreen = Builder.createFavoriteScreenController()
-        favoriteScreen.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "heart"), tag: 2)
+     //   favoriteScreen.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "heart"), tag: 2)
         self.view?.setControllers(controllers: [mainScreen, cameraScreen, favoriteScreen])
     }
     
--- PhotoLibrary/Module/TabBarModul/View/TabBarViewController.swift
+++ PhotoLibrary/Module/TabBarModul/View/TabBarViewController.swift
@@ -14,13 +14,36 @@ protocol TabBarViewControllerProtocol: AnyObject {
 class TabBarViewController: UITabBarController {
     
     var presenter: TabBarViewPresenter!
+  
+    private let tabss : [UIImage] = [.house, .camera, .heart]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemGray
         
+        tabss.enumerated().forEach{
+            let offsets: [Double] = [-100, 0, 100]
+            let tabButton = createTabBarButton(icon: $0.element,
+                                               tag: $0.offset,
+                                               offsetX: offsets[$0.offset],
+                                               isBigButton: $0.offset == 1 ? true : false)
+            
+            view.addSubview(tabButton)
+        }
+        
     }
+    
+    lazy var selectedItem = UIAction { [weak self] sender in
+        guard let self = self,
+              let sender = sender.sender as? UIButton
+        else { return }
+        
+        let tag = sender.tag
+        self.selectedIndex = sender.tag
+        
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
        // self.tabBar.isHidden = true
@@ -33,4 +56,20 @@ extension TabBarViewController: TabBarViewControllerProtocol {
     }
     
     
+}
+
+extension TabBarViewController {
+    private func createTabBarButton(icon: UIImage, tag: Int, offsetX: Double, isBigButton: Bool = false) -> UIButton {
+        
+        return {
+            let btnSize: Double = isBigButton ? 60 : 25
+            $0.frame.size = CGSize(width: btnSize, height: btnSize)
+            $0.tag = tag
+            $0.setBackgroundImage(icon, for: .normal)
+            $0.tintColor = .white
+            $0.frame.origin = CGPoint(x: .zero, y: view.frame.height - (btnSize + (isBigButton ? 44 : 62)))
+            $0.center.x = view.center.x + offsetX
+            return $0
+        }(UIButton(primaryAction: selectedItem))
+    }
 }
