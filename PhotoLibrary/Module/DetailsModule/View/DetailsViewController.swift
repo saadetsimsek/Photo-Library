fad3a782106e662b849f81387ec523c4317221f0
saadetsimsek <saadetsimsek13@gmail.com>
2025-05-14 15:22:24 +0300

    compotional layout

--- PhotoLibrary/Module/DetailsModule/View/DetailsViewController.swift
+++ PhotoLibrary/Module/DetailsModule/View/DetailsViewController.swift
@@ -23,7 +23,17 @@ class DetailsViewController: UIViewController {
         return $0
     }(UIView())
     
- 
+    lazy var navigationHeaderr : NavigationHeader = {
+        NavigationHeader(backAction: backAction,
+                         menuAction: menuAction,
+                         date: presenter.item?.date ?? Date())
+    }()
+    
+    private lazy var headerView: UIView = {
+        navigationHeaderr.getNavigationHeader(type: .back)
+        
+    }()
+    
     lazy var backAction = UIAction { [weak self] _ in
         print("tapped")
         self?.navigationController?.popViewController(animated: true)
@@ -33,23 +43,28 @@ class DetailsViewController: UIViewController {
         print("menu")
     }
     
-    lazy var navigationHeaderr : NavigationHeader = {
-        NavigationHeader(backAction: backAction,
-                         menuAction: menuAction,
-                         date: presenter.item?.date ?? Date())
-    }()
-    
-    private lazy var headerView: UIView = {
-        navigationHeaderr.getNavigationHeader(type: .back)
-    }()
+    private lazy var collectionView: UICollectionView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .none
+        $0.contentInset = UIEdgeInsets(top: 80,
+                                       left: 0,
+                                       bottom: 100,
+                                       right: 0)
+        $0.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        $0.dataSource = self
+        return $0
+    }(UICollectionView(frame: view.bounds, collectionViewLayout: getCompositionalLayout()))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Detail"
         view.backgroundColor = .systemGray
+        view.addSubview(collectionView)
         view.addSubview(topMenuView)
         view.addSubview(headerView)
         setupConstraints()
+       
+     
        
 
     }
@@ -79,4 +94,69 @@ class DetailsViewController: UIViewController {
 
 extension DetailsViewController: DetailViewControllerProtocol {
     
+}
+
+extension DetailsViewController {
+    private func getCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        UICollectionViewCompositionalLayout{ [weak self] section, _ in
+            switch section {
+            default:
+                return self?.createPhotoSection()
+            }
+        }
+    }
+   
+    private func createPhotoSection() -> NSCollectionLayoutSection {
+        //item (size)
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                     leading: 0,
+                                                     bottom: 0,
+                                                     trailing: 30)
+        
+        //group (size) + item
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8),
+                                               heightDimension: .fractionalHeight(0.7))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        //section + group
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                        leading: 30,
+                                                        bottom: 10,
+                                                        trailing: 30)
+        return section
+        
+    }
+}
+
+extension DetailsViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return presenter.item?.photos.count ?? 0
+   /*     case 1:
+            return presenter.item?.tag?.count ?? 0
+        case 2, 4, 5:
+            return 1
+        case 3:
+            return presenter.item?.comments?.count ?? 0 */
+        default:
+            return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
